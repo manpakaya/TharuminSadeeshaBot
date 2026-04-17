e6d8de3394a70fa4bafb82a2749576e05fb3f8da
manpakaya <136682134+manpakaya@users.noreply.github.com>
2026-04-18 02:18:38 +0530

    Update Lara.js

--- Lara.js
+++ Lara.js
@@ -1,6 +1,3 @@
 {
   "zipmegalink": "https://mega.nz/file/X7RVhTTQ#R9HTh_bQdpis7xZqJ9OkaYw9yC74ogPo5dHz8f3-EMA"
-  "version": "4.1.0",
-  "update_url": "https://github.com/manpakaya/TharuminSadeeshaBot/archive/main.zip",
-  "changelog": "Fixed bugs, added new features"
 }
